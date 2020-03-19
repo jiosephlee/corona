d4267a166b3aab4db0e452242a892ebc10719833
Joseph Lee <jlee01@stuy.edu>
2020-03-19 14:24:30 -0400

    updating aesthetics and added live bar

--- corona/static/data.js
+++ corona/static/data.js
@@ -3,24 +3,29 @@ const canvas = document.getElementById('chart');
 const ctx = canvas.getContext('2d');
 
 // chart colors
-var colors = ['#007bff','#28a745','#333333','#c3e6cb','#dc3545','#6c757d'];
+var colors = ['#FF0000','#FF6347','#007bff','#28a745','#333333','#c3e6cb','#dc3545','#6c757d'];
 
 /* large line chart */
 var chartData = {
-  labels: ["3/5","3/6","3/7","3/8", "3/9","3/10", "3/11 10:30AM","3/12 4:00PM", "3/13 2:00PM", "3/14 1:30PM", "3/15 12:00PM", "3/16 11:30AM", "3/17 10:30AM", "3/18 6:00PM"],
+  labels: ["3/1","3/2","3/3","3/4","3/5","3/6","3/7","3/8", "3/9","3/10", "3/11","3/12", "3/13", "3/14", "3/15", "3/16", "3/17", "3/18"],
   datasets: [{
-    data: [3,4,11,12,19,35, 46, 95, 154, 183, 269, 463, 644 , 1871],
+    data: [1,1,2,2,4,7,12,14,25,32, 53, 88, 137, 185, 269, 464, 923, 1871],
     backgroundColor: 'transparent',
-    borderColor: colors[0],
+    borderColor: colors[1],
     borderWidth: 2,
     pointRadius: 4,
-    pointBackgroundColor: colors[1]
+    pointBackgroundColor: colors[0]
   }]
 };
 new Chart(ctx, {
     type: 'line',
     data: chartData,
     options: {
+      title: {
+            display: true,
+            Color: colors[1],
+            text: 'The Curve'
+        },
       scales: {
         xAxes: [{
           ticks: {
