550a3716e9d034d30b672d5bcdc309fc33789496
Joseph Lee <jlee01@stuy.edu>
2020-03-20 18:53:16 -0400

    EST update

--- corona/__init__.py
+++ corona/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, render_template, redirect, url_for, session, flash
-from datetime import datetime, timedelta
+from datetime import datetime
+from pytz import timezone
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    now = datetime.now()
+    tz = timezone('EST')
+    datetime.now(tz)
+    now = datetime.now(tz)
     if (int(now.strftime("%H")) == 12):
         current_time = now.strftime("%H:%M:%S") + " AM"
     elif (int(now.strftime("%H")) > 12):
