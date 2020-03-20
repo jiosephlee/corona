64bf2a9ae90ce6cad848e536e2b249d9c3dd4f5b
Joseph Lee <jlee01@stuy.edu>
2020-03-20 19:00:14 -0400

    fixing

--- corona/__init__.py
+++ corona/__init__.py
@@ -6,9 +6,8 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    tz = timezone('EST')
-    datetime.now(tz)
-    now = datetime.now(tz)
+    eastern = timezone('US/Eastern')
+    now = datetime.now(eastern)
     if (int(now.strftime("%H")) == 12):
         current_time = now.strftime("%H:%M:%S") + " AM"
     elif (int(now.strftime("%H")) > 12):
